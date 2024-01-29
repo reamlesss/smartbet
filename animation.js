87a525c5db522b0a050e48c92a09cfe5303259aa
Václav Vondráček <108518865+reamlesss@users.noreply.github.com>
2024-01-29 11:40:38 +0100

    re-add

--- animation.js
+++ animation.js
@@ -6,26 +6,22 @@ const observer = new IntersectionObserver((entries) => {
 
             switch(animationType){
                 case 'slide-left':
-                    entry.target.classList.add('slide-left');
+                    entry.target.classList.add('slide-left')
                 case 'scaleUp':
-                    entry.target.classList.add('scaleUp');
+                    entry.target.classList.add('scaleUp')
                 case 'slide-right':
-                    entry.target.classList.add('slide-right');
-                case 'slide-up':
-                    entry.target.classList.add('slide-up');
+                    entry.target.classList.add('slide-right')
             }
 
         }
     })
 })
 
-const hiddenElements = document.querySelectorAll('.hidden');
-hiddenElements.forEach((el) => observer.observe(el))
+
+
 const leftHiddenElements = document.querySelectorAll('.hidden-left');
 leftHiddenElements.forEach((el) => observer.observe(el))
 const rightHiddenElements = document.querySelectorAll('.hidden-right');
 rightHiddenElements.forEach((el) => observer.observe(el))
 const scaleHiddenElements = document.querySelectorAll('.hidden-scale');
 scaleHiddenElements.forEach((el) => observer.observe(el))
-const downHiddenElements = document.querySelectorAll('.hidden-down');
-downHiddenElements.forEach((el) => observer.observe(el))
